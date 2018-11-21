5ea862b3c842f27585391bf9f95df294c7fe0415
matthewwolff <mwolff3@wisc.edu>
2018-11-21 16:22:02 -0600

    compared filtering btwn fastp and bbduk

--- CHTC/scripts/filtering.sh
+++ CHTC/scripts/filtering.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e -x
 GLUST=/mnt/gluster/mwolff3
-for file in `ls $GLUST/*.fastq.tar.gz`; do # list all files 
-  if [[ ! -f $GLUST/${file%.*}_filtered.tgz ]]; then # make sure hasn't been filtered 
-    echo `basename $file .fastq.tar.gz`
-  fi  
-done > unfiltered_files
-METAGENOMES=$(cat unfiltered_files) && rm unfiltered_files
+METAGENOME=`basename $1 .fastq.tar.gz` # filename should be argument
+echo "filtering $METAGENOME" && echo
 
 # java installation
 tar -xzf $GLUST/jdk-10_linux-x64_bin.tar.gz
@@ -26,13 +22,9 @@ echo "untarred bbmap" && echo
 
 # transfer & filter metagenomes, then move to gluster
 echo "beginning filtering..."
-mkdir -p metagenomes/
-for raw in $METAGENOMES; do
-  tar -xzf $GLUST/${raw}.fastq.tar.gz -C metagenomes/ # transfer in
-  input=metagenomes/${raw}.fastq; output=${raw}_filtered.fastq
-  bbduk.sh in=$input out=$output qtrim=r trimq=10 maq=10 # filter! 
-  # efficient disk usage + naming -> won't read in .tgz files -> won't mistakenly read filtered
-  rm $input && tar -czvf $GLUST/${output}.tgz $output --remove-files 
-done
-echo "filtered metagenomes!"
-exit
+mkdir -p metagenome/
+tar -xzf $GLUST/${METAGENOME}.fastq.tar.gz -C metagenome/ # transfer in
+input=metagenome/${METAGENOME}.fastq; output=metagenome/${METAGENOME}_filtered.fastq
+bbduk.sh in=$input out=$output qtrim=r trimq=10 maq=10 # filter! 
+rm $input && tar -czvf $GLUST/$(basename $output).tgz $output --remove-files 
+echo "filtered metagenome!"
